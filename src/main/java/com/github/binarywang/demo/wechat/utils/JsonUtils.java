9a4036c8fa946bc48a0c0d35c15be71d410a91a0
Binary Wang <binarywang@gmail.com>
2017-07-14 11:04:06 +0800

    exclude jackson to avoid some mistakes

--- src/main/java/com/github/binarywang/demo/wechat/utils/JsonUtils.java
+++ src/main/java/com/github/binarywang/demo/wechat/utils/JsonUtils.java
@@ -1,28 +1,12 @@
 package com.github.binarywang.demo.wechat.utils;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
 
 /**
  * @author Binary Wang(https://github.com/binarywang)
  */
 public class JsonUtils {
-    private static final ObjectMapper JSON = new ObjectMapper();
-
-    static {
-        JSON.setSerializationInclusion(Include.NON_NULL);
-        JSON.configure(SerializationFeature.INDENT_OUTPUT, Boolean.TRUE);
-    }
-
     public static String toJson(Object obj) {
-        try {
-            return JSON.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return WxMpGsonBuilder.create().toJson(obj);
     }
 }
