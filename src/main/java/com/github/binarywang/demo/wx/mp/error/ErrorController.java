7c15eaf441e5c85183c31c7b00a76c4861a02975
Binary Wang <binarywang@gmail.com>
2018-08-27 00:00:42 +0800

    spring boot版本升级2.0.4

---
+++ src/main/java/com/github/binarywang/demo/wx/mp/error/ErrorController.java
@@ -0,0 +1,29 @@
+package com.github.binarywang.demo.wx.mp.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <pre>
+ * 出错页面控制器
+ * Created by Binary Wang on 2018/8/25.
+ * </pre>
+ *
+ * @author <a href="https://github.com/binarywang">Binary Wang</a>
+ */
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+  @GetMapping(value = "/404")
+  public String error404() {
+    return "error";
+  }
+
+  @GetMapping(value = "/500")
+  public String error500() {
+    return "error";
+  }
+
+}
---
+++ src/main/java/com/github/binarywang/demo/wx/mp/error/ErrorPageConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.binarywang.demo.wx.mp.error;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * <pre>
+ * 配置错误状态与对应访问路径
+ * Created by Binary Wang on 2018/8/25.
+ * </pre>
+ *
+ * @author <a href="https://github.com/binarywang">Binary Wang</a>
+ */
+@Component
+public class ErrorPageConfiguration implements ErrorPageRegistrar {
+  @Override
+  public void registerErrorPages(ErrorPageRegistry errorPageRegistry) {
+    errorPageRegistry.addErrorPages(
+        new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"),
+        new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500")
+    );
+  }
+
+}
